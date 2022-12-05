77d48a3440a748a1d0ee674665eb8f66c8c25cd1
Parker Higgins <parker@parkerhiggins.net>
2022-12-05 15:13:54 -0500

    improved input parsing, better variable names

--- 05/crates.py
+++ 05/crates.py
@@ -5,34 +5,32 @@ def move_stacks(stacks, instructions, multiple=False):
         p = i.split()
         """p[1] is qty, p[3] is src, p[5] is dst"""
 
-        p[1], p[3], p[5] = int(p[1]), int(p[3]), int(p[5])
+        qty, src, dst = int(p[1]), int(p[3]), int(p[5])
 
-        moved_blocks = stacks[p[3]][:p[1]]
+        moved_blocks = stacks[src][:qty]
         if not multiple:
             moved_blocks.reverse()
 
-        stacks[p[5]] = moved_blocks + stacks[p[5]]
-        stacks[p[3]] = stacks[p[3]][p[1]:]
+        stacks[dst] = moved_blocks + stacks[dst]
+        stacks[src] = stacks[src][qty:]
     
     return stacks
 
 def main():
     stacks = {}
+    instructions = []
     with open('input') as f:
         for l in [line.strip('\n') for line in f.readlines()]:
-            if l.startswith(' 1'):
-                break
-            for x in range(0, 9):
-                stacks[x+1] = stacks.get(x+1, [])
-                value = l[x*4:x*4+3]
-                if value != '   ':
-                    stacks[x+1].append(value)
-
-    with open('input') as f: #lol
-        instructions = []
-        for l in [line.strip('\n') for line in f.readlines()]:
-            if l.startswith('move'):
+            if not l or l.startswith(' 1'):
+                continue
+            elif l.startswith('move'):
                 instructions.append(l)
+            else:
+                for x in range(0, 9):
+                    stacks[x+1] = stacks.get(x+1, [])
+                    value = l[x*4:x*4+3]
+                    if value != '   ':
+                        stacks[x+1].append(value)
 
     restacked = move_stacks(stacks.copy(), instructions)
 
