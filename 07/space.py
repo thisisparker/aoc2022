e37cdef5ffb8b53244fbde12b516395ce1c80162
Parker Higgins <parker@parkerhiggins.net>
2022-12-07 15:35:49 -0500

    a few more revisions just to make things look a little nicer

--- 07/space.py
+++ 07/space.py
@@ -3,22 +3,26 @@ def main():
         output = [l.strip() for l in f.readlines()][1:]
 
     tree = {}
-    pwd = '/'
-    current_view = None
+    parent_dirs = ['/']
     for i in output:
+        pwd = parent_dirs[-1]
         if i.startswith('$ cd'):
             dst = i.split()[2]
-            pwd = pwd + dst + '/' if dst != '..' else pwd.rsplit('/', 2)[0] + '/'
+            if dst == '..':
+                parent_dirs.pop()
+            else:
+                parent_dirs.append(pwd + dst + '/')
+
         elif i.startswith('$ ls'):
             continue
+
         else:
             size_or_type, name = i.split()
-            if size_or_type.isnumeric():
-                parent_dirs = []
-                for slash in range(1, pwd.count('/') + 1):
-                    parent_dirs.append(pwd.rsplit('/', slash)[0] + '/')
-                for d in parent_dirs:
-                    tree[d] = (tree.get(d, 0) + int(size_or_type))
+            if not size_or_type.isnumeric():
+                continue
+
+            for d in parent_dirs:
+                tree[d] = tree.get(d, 0) + int(size_or_type)
 
     small_dirs = [d for d in tree if tree[d] <= 100000]
 
@@ -29,7 +33,6 @@ def main():
 
     print(min([tree[d] for d in tree if tree[d] >= target]))
 
-        
 
 if __name__ == '__main__':
     main()
